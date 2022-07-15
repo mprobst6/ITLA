5652786c8bdd2173cce61d299b228f8a799afba6
Michael Probst <probst_michael@verizon.net>
2022-07-15 10:44:42 -0400

    turning on and off functioning properly

---
+++ response.py
@@ -0,0 +1,6 @@
+error_messages = {
+    '0' : 'OK flag (normal return status)',
+    '1' : 'XE flag (execution error)',
+    '2' : 'AEA flag (automatic extended addressing result being returned or ready to write)',
+    '3' : 'CP flag (command not complete, pending)'
+}
